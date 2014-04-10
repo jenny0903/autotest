51bd87c1016d932f7b9acf5807927583e3ef7074
jenny <18818225383@163.com>
2014-04-10 17:57:38 +0800

    push 02

--- api_test.php
+++ api_test.php
@@ -1,6 +1,4 @@
 <?php
-include_once('curl.php');
-
 $api = $_POST['api'];
 
 $api =  json_decode($api,true);
--- scheme_test.php
+++ scheme_test.php
@@ -1,6 +1,4 @@
 <?php
-include_once('curl.php');
-
 $con = mysql_connect("10.32.1.6","root","root");
 if (!$con){
 	die('Could not connect: ' . mysql_error());
@@ -8,72 +6,93 @@ if (!$con){
 mysql_select_db("cliq_auto_test", $con);
 
 $sch_flag = 0;
-
+$api_success = 0;
+$api_fail = array();
+$api_code = array();
+$sch_success = array();
+$sch_code = array();
+$time_code = array();
 // $schemes = $_POST['scheme'];
 
 // $schemes = json_decode($schemes,true);
 // foreach($schemes as $value01){
-	// $scheme = $value['scheme'];
-	// $time = $value['test_time'];
+	// $scheme_id = $value['scheme'];
+	// $sch_time = $value['test_time'];
 	$scheme = "EBCFF6A0-742F-7AB0-32DE-90175D1B51E4";
-	
-	$sql01 = "SELECT * FROM scheme WHERE scheme_id='$scheme'";
+	//取方案
+	$sql01 = "SELECT * FROM scheme WHERE scheme_id='$scheme_id'";
 	
 	$result01 = mysql_query($sql01);
 
 	if(mysql_num_rows($result01) > 0){
-		// foreach(mysql_fetch_array($result01) as $value02){
-			// echo $value02['api_id'].'**'.$value02['api_time'];
-			// exit;
-		// }
-		// while($row = mysql_fetch_array($result01)){
-			// echo $row['api_id'].'**'.$row['api_time'];
-		// }
-		while($row = mysql_fetch_assoc($result01)){
-		   $scheme_tmp_array[] = $row;
+		//循环执行方案
+		for(var j=0; j<$sch_time ; j++){
+			//遍历方案，获取api
+			while($row = mysql_fetch_array($result01)){
+				// echo $row['api_id'].'**'.$row['api_time'];
+				$api = $row['api_id'];
+				$api_time = $row['api_time'];
+
+					//循环执行api
+					for(var i=0; i<$api_time; i++){
+						switch($api){
+							case 1:
+								$result = getuserinfo($uid,$token);
+								break;
+							case 2:
+								break;
+						}			
+						$code = $result['code'];
+						//保存该次api的单次结果
+						if($code == 200){
+							// $data['code']['success'] += 1; 
+							$api_success++; 
+						}else{
+							$api_fail[] = array(
+								"object_id" => i,
+								"object_code" => $code,
+								"object_data" => $result['data']
+							);
+						}
+						
+					}
+					//保存执行api的结果
+					$api_code[] = array(
+							"success" => $api_success,
+							"fail" => $api_fail
+					);
+					if($api_success != $api_time ){
+						$time_code[] = array(
+							"api_id" => $api,
+							"api_code" => $api_code
+						);
+					}else{
+						$sch_success++;
+					}
+					//api中介参数置0
+					$api_success = 0;
+					$api_fail = array();
+			}
+
+			//保存执行方案的单次结果
+			if($sch_success != $sch_time ){
+				$sch_code[] = array(
+					"time" => j,
+					"time_code" => $time_code
+				);
+			}else{
+				$success++;
+			}
 		}
-		echo "<pre>";
-		var_dump($scheme_tmp_array);
-		exit;
+		//保存方案的执行结果
+		$data = array(
+			"id" => $scheme,
+			"sch_code" => $sch_code,
+			"success" => $success,
+			"fail" => ($sch_time - $success)
+		);
 	}
 // }
 
-
-// $data = array();
-// $sch_success = 0;
-// $sch_fail = array();
-// foreach($schemes as $value){
-	// $scheme = $value['scheme'];
-	// $time = $value['test_time'];
-	
-	// for(var i = 0; i < $time; i++){
-		// switch($api){
-			// case 1:
-				// $result = getuserinfo($uid,$token);
-				// break;
-			// case 2:
-				// break;
-		// }
-		// $code = $result['code'];
-		// if($code == 200){
-			// $data['code']['success'] += 1; 
-			// $success++; 
-		// }else{
-			// $fail[] = array(
-				// "object_id" => i,
-				// "object_code" => $code,
-				// "object_data" => $result['data']
-			// );
-		// }
-	// }
-	// $data[] = array(
-		// "success" => $success,
-		// "fail" => $fail
-	// );
-	// $success = 0;
-	// $fail = array();
-// }
-
-// echo JSON($data);
 
 ?> 
